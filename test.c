6be31d1d5b1fbdc9fc88b6527f921c3bb6536dac
unknown <PNTINTERN2@PNT-INTERN2.indiannica.net>
2022-06-27 12:11:58 +0530

    factorial program is created with loop

---
+++ test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int i,n,fact=1;
+    printf("enter the factoral of a given no.");
+    scanf("%d\n",&n);
+    for(i=1;i<=n;i++)
+    {
+        fact=fact*i;
+    }
+    printf("\nfactorial of a %d is %d",n,fact);
+}
