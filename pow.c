bbcfda281bfd7a675195e1aa39e446678f36351b
unknown <PNTINTERN2@PNT-INTERN2.indiannica.net>
2022-06-30 16:44:41 +0530

    power program of any no. first

---
+++ pow.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,x,pow=1;
+    scanf("%d %d",&n,&x);
+    for(int i=1;i<=x;i++)
+    {
+        pow=pow*n;
+    }
+    printf("%d",pow);
+ return 0;   
+}
+t.c = o(n)
